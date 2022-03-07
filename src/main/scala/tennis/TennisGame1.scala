38ea924589a19640cfc607b4ecad376256fcd957
Roy Straub <roy.straub@codesquad.nl>
2022-03-07 18:07:52 +0100

    Variable name

--- src/main/scala/tennis/TennisGame1.scala
+++ src/main/scala/tennis/TennisGame1.scala
@@ -2,37 +2,37 @@ package tennis
 
 class TennisGame1(val player1Name: String, val player2Name: String)
     extends TennisGame {
-  private var m_score1: Int = 0
-  private var m_score2: Int = 0
+  private var playerOneScore: Int = 0
+  private var playerTwoScore: Int = 0
 
   def wonPoint(playerName: String): Unit = {
     if (playerName == "player1")
-      m_score1 += 1
+      playerOneScore += 1
     else
-      m_score2 += 1
+      playerTwoScore += 1
   }
 
   def calculateScore(): String = {
     var score: String = ""
     var tempScore = 0
-    if (m_score1 == m_score2) {
-      score = m_score1 match {
+    if (playerOneScore == playerTwoScore) {
+      score = playerOneScore match {
         case 0 => "Love-All"
         case 1 => "Fifteen-All"
         case 2 => "Thirty-All"
         case _ => "Deuce"
 
       }
-    } else if (m_score1 >= 4 || m_score2 >= 4) {
-      val minusResult = m_score1 - m_score2
+    } else if (playerOneScore >= 4 || playerTwoScore >= 4) {
+      val minusResult = playerOneScore - playerTwoScore
       if (minusResult == 1) score = "Advantage player1"
       else if (minusResult == -1) score = "Advantage player2"
       else if (minusResult >= 2) score = "Win for player1"
       else score = "Win for player2"
     } else {
       for (i <- 1 until 3 by 1) {
-        if (i == 1) tempScore = m_score1
-        else { score += "-"; tempScore = m_score2; }
+        if (i == 1) tempScore = playerOneScore
+        else { score += "-"; tempScore = playerTwoScore; }
         val tempScore2 = tempScore match {
           case 0 => "Love"
           case 1 => "Fifteen"
