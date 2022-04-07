995f1579afcbea20f657465069e454192592c299
Roy <roy@codecraftr.nl>
2022-04-07 11:54:18 +0200

    Result variable

--- src/main/scala/tennis/TennisGame3.scala
+++ src/main/scala/tennis/TennisGame3.scala
@@ -4,22 +4,23 @@ class TennisGame3(val playerOneName: String, val playerTwoName: String)
     extends TennisGame {
   private val playerOne = new Player(playerOneName)
   private val playerTwo = new Player(playerTwoName)
+
   def calculateScore(): String = {
-    var s: String = ""
+    var result: String = ""
     if (playerOne.points < 4 && playerTwo.points < 4 && !(playerOne.points + playerTwo.points == 6)) {
       val p = Array("Love", "Fifteen", "Thirty", "Forty")
-      s = p(playerOne.points)
-      if (playerOne.points == playerTwo.points) s + "-All"
-      else s + "-" + p(playerTwo.points)
+      result = p(playerOne.points)
+      if (playerOne.points == playerTwo.points) result + "-All"
+      else result + "-" + p(playerTwo.points)
     } else {
       if (playerOne.points == playerTwo.points) "Deuce"
       else {
-        s =
+        result =
           if (playerOne.points > playerTwo.points) playerOneName
           else playerTwoName
         if ((playerOne.points - playerTwo.points) * (playerOne.points - playerTwo.points) == 1)
-          "Advantage " + s
-        else "Win for " + s
+          "Advantage " + result
+        else "Win for " + result
       }
     }
   }
